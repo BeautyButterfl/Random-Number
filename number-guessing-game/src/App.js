0c097ac5cd433ddf1cd9f91aa545b62e01bfb84a
Your Name <you@example.com>
2024-10-16 17:38:56 +0800

    Initial commit with number guessing game

---
+++ number-guessing-game/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GameNumber from './guess';
+
+function App() {
+  return (
+    <div className="App">
+      <GameNumber />
+    </div>
+  );
+}
+
+export default App;
+
+
